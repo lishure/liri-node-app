abfb899dda3fe2a2849852e12bbcd48c2476c6d8
Lishure <HUEY_LISA@YAHOO.COM>
2019-04-29 21:47:35 -0700

    psuedocode

--- liri.js
+++ liri.js
@@ -1,3 +1,22 @@
+//Read and set environment variables
 require("dotenv").config();
+
+//Create variables
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+//Capture User Inputs
+
+//Create function for node commands
+
+//Create function for Spotify
+//Need queryURL + apikey
+
+//Create function for Bands In Town
+
+//Need queryURL+apikey
+
+//Create function for OMDB
+
+//Need queryURL+ api key
+
